0dde60c6e73ca2d15958513fe41e661890e170c7
Romel Zavala <rzavala@megatk.com>
2018-08-04 00:45:17 -0600

    Update account.py

--- sar/models/account.py
+++ sar/models/account.py
@@ -5,6 +5,6 @@ from odoo import models, fields
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
-
+#romel
     allow_multi_sequence = fields.Boolean('Multisecuencias Fiscales', default=False, help='Create multi sequences for this journal')
     sequence_ids = fields.Many2many('ir.sequence', string='Secuencias')
